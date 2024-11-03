de38e7cff75e9c9baf0fcec9173381370c5c9816
bkbk_b <1320832053@qq.com>
2024-11-03 22:32:21 +0800

    add lcm config in container_init.sh

--- Docker/container_init.sh
+++ Docker/container_init.sh
@@ -1,6 +1,10 @@
 #modify mavros
 sed -i 's|/dev/ttyACM0:57600|/dev/ttyTHS0:921600|1' /opt/ros/noetic/share/mavros/launch/px4.launch
 
+#LCM Config
+ifconfig wlan1 multicast
+route add -net 224.0.0.0 netmask 240.0.0.0 dev wlan1
+
 #start service
 service ssh start
 /daemon/nvargus-daemon &
