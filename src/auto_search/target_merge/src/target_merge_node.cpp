a01a85eea3bfac78c4c36626b9368cd2b415b3e3
bkbk_b <1320832053@qq.com>
2024-11-05 22:04:38 +0800

    add the ability to determine whether the target is reliable

--- src/auto_search/target_merge/src/target_merge_node.cpp
+++ src/auto_search/target_merge/src/target_merge_node.cpp
@@ -6,9 +6,8 @@ using namespace target_merge;
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "target_merge_node");
-  ros::NodeHandle nh;
   Target_Merge tm;
-  tm.init(nh);
+  tm.init();
   ros::spin();
   return 0;
 }
