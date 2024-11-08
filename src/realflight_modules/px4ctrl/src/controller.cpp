ad8579eda4cbb14de638c145874c2b00acc4e900
Longer95479 <371573369@qq.com>
2024-11-08 15:09:41 +0800

    add thr and thr2acc print

--- src/realflight_modules/px4ctrl/src/controller.cpp
+++ src/realflight_modules/px4ctrl/src/controller.cpp
@@ -160,8 +160,10 @@ LinearControl::estimateThrustModel(
     double K = gamma * P_ * thr;
     thr2acc_ = thr2acc_ + K * (est_a(2) - thr * thr2acc_);
     P_ = (1 - K * thr) * P_ / rho2_;
-    // printf("%6.3f,%6.3f,%6.3f,%6.3f,%6.3f,%6.3f\n", est_a(2), thr, thr2acc_, gamma, K, P_);
-    // fflush(stdout);
+    if (param_.thr_map.print_val == true) {
+      printf("%6.3f,%6.3f,%6.3f,%6.3f,%6.3f,%6.3f\n", est_a(2), thr, thr2acc_, gamma, K, P_);
+      fflush(stdout);
+    }
 
     debug_msg_.thr_scale_compensate = thr2acc_;
     return true;
